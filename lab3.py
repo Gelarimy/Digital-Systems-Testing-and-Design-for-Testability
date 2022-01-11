ba852cdbc3f081447756ebb33b5713bde49b2409
Catherine <66085925+Gelarimy@users.noreply.github.com>
2022-01-11 20:52:27 +0300

    TEST GENERATORS BASED ON SHIFT REGISTERS WITH LINEAR FEEDBACK

---
+++ lab3.py
@@ -0,0 +1,43 @@
+
+# import LFSR
+import numpy as np
+from pylfsr import LFSR
+
+
+def LFSR():
+    D1
+    D2
+    D3
+    D4
+    D5
+    D6
+    D7
+    D8
+
+    #D1 = D8 xor D
+    D2
+    D3
+    D4
+    D5
+    D6
+    D7
+    D8
+
+def LFSR():
+    L = LFSR()
+
+    # print the info
+    L.info()
+
+    L.next()
+    L.runKCycle(10)
+    L.runFullCycle()
+    L.info()
+
+    L.Viz(show=False, show_labels=False, title='R1')
+
+
+
+
+if __name__ == '__main__':
+    print(1^0)
